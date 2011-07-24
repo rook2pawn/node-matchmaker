b4ac6a0045ca04d25321ab8aa94e9f063c8d0bb0
david wee <rook2pawn@gmail.com>
2011-07-23 17:45:43 -0700

    Updated how comparison function and result sets look like for a closeness/similarity function

--- test.js
+++ test.js
@@ -1,46 +1,74 @@
 var matcher = require('./index');
+// so conceptually, the way to write a preference fn -> [0%,100%] is
+// to have an idealized opponent, and then return the percentage that your
+// current possible opponent matches this idealized opponent.
+
+// so this preference function has an idealized opponent that matches ones own ELO
+// thus the closer the opp is to him/her/itself then the closer it is to 100%.
 var preferenceFn = function(other,self) {
 	console.log("other elo: " + other.elo);
 	console.log("self.elo :"  + self.elo);
-	var diff = (other.elo - self.elo);
-	if (diff >= 0) return 1;
-	else return 0.5;	
+//2100 - 2100 / 2100 = 0% diff,  -> 100% - 0% = 100% match
+//2300 - 2100 / 2100 = 10% difference.-> 100% - 10% = 90% match
+	var diff = Math.abs(other.elo - self.elo);
+	var ratio = diff / self.elo;
+	var match = 1 - ratio;
+	return match;
+
 };
 var generateResultSetFn = function() {
 };
 matcher()
-	.push({id:1,name:"Garry Kasaparov",elo:2100},
+	.push({id:1,name:"Garry Kasaparov",elo:2100,win:190,loss:20},
 	function(el,emitter,self) { 
 		setTimeout(function() {
-			console.log(el);
-			console.log(self.queue());
-		var queue = self.queue();
-		var resultSet = [];
-		queue.forEach(function(obj) {
-			console.log("Foreach..");console.log(obj[0]);		
-			console.log(preferenceFn(obj[0],el));
-		});
+			var queue = self.queue();
+			var resultSet = [];
+			queue.forEach(function(obj) {
+				var result = {match:preferenceFn(obj[0],el),player:obj};
+				resultSet.push(result);
+			});
+			console.log("Result set for " + el.name); console.log(resultSet);
+			emitter.emit('next');
+		},1000);
+	})
+	.push({id:2,name:"Bobby Fischer",elo:2050,win:100,loss:38},
+	function(el,emitter,self) { 
+		setTimeout(function() {
+			var queue = self.queue();
+			var resultSet = [];
+			queue.forEach(function(obj) {
+				var result = {match:preferenceFn(obj[0],el),player:obj};
+				resultSet.push(result);
+			});
+			console.log("Result set for " + el.name); console.log(resultSet);
 
 			emitter.emit('next');
 		},1000);
 	})
-	.push({id:2,name:"Bobby Fischer",elo:2050},
+	.push({id:3,name:"Glaurung",elo:2800,win:129,loss:33},
 	function(el,emitter,self) { 
-		console.log(el);
-		console.log(self.queue());
-		emitter.emit('next');
+		setTimeout(function() {
+			var queue = self.queue();
+			var resultSet = [];
+			queue.forEach(function(obj) {
+				var result = {match:preferenceFn(obj[0],el),player:obj};
+				resultSet.push(result);
+			});
+			console.log("Result set for " + el.name); console.log(resultSet);
+			emitter.emit('next');
+		},1000);
 	})
-	.push({id:3,name:"Glaurung",elo:2800},
+	.push({name:"Fruit",elo:2750,win:201,loss:10},
 	function(el,emitter,self) { 
-		console.log(el);
-		console.log(self.queue());
-//		self.forEach(function(val){
-//			console.log("DFDF");console.log(val)});
-		emitter.emit('next');
-	})
-	.push({name:"Fruit",elo:2750},
-	function(el,emitter,self) { 
-		console.log(el);
-		console.log(self.queue());
-		emitter.emit('next');
+		setTimeout(function() {
+			var queue = self.queue();
+			var resultSet = [];
+			queue.forEach(function(obj) {
+				var result = {match:preferenceFn(obj[0],el),player:obj};
+				resultSet.push(result);
+			});
+			console.log("Result set for " + el.name); console.log(resultSet);
+			emitter.emit('next');
+		},1000);
 	});
