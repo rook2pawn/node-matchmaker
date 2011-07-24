eeb6727fbdc33fc11e6b3fd04b528bad388f4756
david wee <rook2pawn@gmail.com>
2011-07-24 05:35:06 -0700

    put the preference and similarity functions staple included functions on the library

--- index.js
+++ index.js
@@ -20,10 +20,33 @@ var Matcher = function(obj) {
 	};
 	return self;
 };
-
+Matcher.similarityFn = function(other,self,key) {
+	var diff = Math.abs(other[key]- self[key]);
+	var ratio = diff / self[key];
+	var match = 1 - ratio;
+	return match;
+};
+Matcher.preference = function(el,emitter,self) { 
+	var queue = self.queue();
+	var resultSet = [];
+	queue.forEach(function(obj) {
+		if (obj[0].id !== el.id) {
+			var result = {match:Matcher.similarityFn(obj[0],el,'elo'),player:obj};
+			resultSet.push(result);
+		}
+	});
+	if (resultSet.length <= 3) { 
+		setTimeout(function() {
+			Matcher.preference(el,emitter,self);
+		},1000);
+	} else {
+		var comp = function(a,b) {
+			return b.match - a.match
+		};
+		el.resultSet = resultSet.sort(comp);
+		var opp = el.resultSet[0].player[0];
+		console.log(el.name + "("+ el.elo + ") wants to be matched against " + opp.name + "(" + opp.elo + ")");
+	}
+	emitter.emit('next');
+};
 exports = module.exports = Matcher;
-
-// an evalutation function will be passed one object, and the sole purpose of this function
-// f(obj) = numerical value. 
-// however! the evaluation can and should be partially applied to preconfigure any global additional parameters without
-// having be overly specific in the per-user evaluation function
